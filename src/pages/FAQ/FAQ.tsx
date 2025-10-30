6e49c8a88f5aaa9a54615b74f5aa9bdbe94607fb
Mayara Bispo <mayarabispo.orangebox@gmail.com>
2025-10-30 14:56:20 -0300

    Setup initial folder structure

---
+++ src/pages/FAQ/FAQ.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './FAQ.module.css';
+
+export const FAQ: React.FC = () => {
+  return (
+    <div>
+      <h1>Perguntas Frequentes</h1>
+    </div>
+  );
+};
